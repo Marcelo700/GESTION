297f1427ac9f947fe49b123ec01fc1448b20126f
Marcelo700 <katomogollon@gmail.com>
2025-05-07 12:49:57 -0500

    seguir con el video

--- Assets/js/pages/file.js
+++ Assets/js/pages/file.js
@@ -1,6 +1,13 @@
 const btnUpload = document.querySelector('#btnUpload')
+const modalFile = document.querySelector("#modalFile");
+const modalFile = document.querySelector("#modalFile");
+const myModal = new bootstrap.Modal(modalFile);
+
+const modalCarpeta = document.querySelector("#modalCarpeta");
+const myModal1 = new bootstrap.Modal(modalCarpeta);
+
 document.addEventListener('DOMContentLoaded', function(){
     btnUpload.addEventListener('click', function(){
-        
+        myModal.show();
     })
 })
