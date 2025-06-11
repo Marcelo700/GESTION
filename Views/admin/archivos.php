9271b626605b76f0c0fc4f75b5a10787cef4ee7d
Marcelo700 <katomogollon@gmail.com>
2025-06-11 12:26:48 -0500

    no elimina del todo

--- Views/admin/archivos.php
+++ Views/admin/archivos.php
@@ -21,13 +21,10 @@
                                 <div class="d-flex align-items-center">
                                     <i class="material-icons-outlined text-danger align-middle m-r-sm">description</i>
                                     <a href="#" class="file-manager-recent-item-title flex-fill"><?php echo $archivo['nombre']; ?></a>
-                                    <span class="p-h-sm">167kb</span>
-                                    <span class="p-h-sm text-muted">09.14.21</span>
                                     <a href="#" class="dropdown-toggle file-manager-recent-file-actions" id="file-manager-recent-<?php echo $archivo ['id']; ?>" data-bs-toggle="dropdown" aria-expanded="false"><i class="material-icons">more_vert</i></a>
                                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="file-manager-recent-<?php echo $archivo ['id']; ?>">
-                                        <li><a class="dropdown-item" href="#">Compartir</a></li>
-                                        <li><a class="dropdown-item" href="#">Descargar</a></li>
-                                        <li><a class="dropdown-item" href="#">Eliminar</a></li>
+                                        <li><a class="dropdown-item" href="<?php echo BASE_URL . 'Assets/archivos/' . $archivo['id_carpeta'] . '/' . $archivo['nombre']; ?>" download="<?php echo $archivo['nombre'];?>">Descargar</a></li>
+                                        <li><a class="dropdown-item eliminar" href="#" data-id="<?php echo $archivo['id']; ?>">Eliminar</a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -41,5 +38,8 @@
 
 
 
-<?php include_once 'Views\template\footer.php'; ?>
+<?php
+ 
+include_once 'Views\template\footer.php'; 
+?>
 
